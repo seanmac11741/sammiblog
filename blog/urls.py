5209c407b7963f9a37ff85446b9312effaf32ae9
Sean McConnell <smcconn3@uwyo.edu>
2019-08-21 10:45:30 -0600

    template for django blog. Want to change the bootstrap and css ALOT now

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,11 @@
 #created in part2
 from django.urls import path
-from . import views #this imports something from current directory, that is views.py that we just made
-
+from . import views
 
 urlpatterns = [
     #dont need any path, because this is for all blog sites
-    path('', views.home, name='blog-home'), #sends it to views.home where views is the thing we imported
-    # path('', PostListView.as_view(), name='blog-home')
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='about'),
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 
+posts = [
+    {
+    'author': 'Sean',
+    'title': 'Blog Post 1',
+    'content': 'first post content',
+    'date_posted': 'August 23'
+    },
+    {
+    'author': 'Sean2',
+    'title': 'Blog Post 2',
+    'content': '2 post content',
+    'date_posted': 'August 20'
+    }
+]
+
 # Create your views here.
 def home(request):
-    # context = {
-    #     'posts': Post.objects.all()
-    # }
-    # return render(request, 'blog/home.html', context) #3rd param is passed to the template and the template can access it
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context) #3rd param is passed to the template and the template can access it
+    # return render(request, 'blog/home.html')
+
+def about(request):
+    return render(request, 'blog/about.html', {'title':'About'})
